f245955c0d33661e7e71ed3663506a392db4181e
Julio Augusto <julioaugustov@gmail.com>
2025-12-11 20:57:35 -0300

    adicionado comentários a classe Veiculo

--- src/br/senai/sp/jandira/estacionamentoApp/model/Veiculo.java
+++ src/br/senai/sp/jandira/estacionamentoApp/model/Veiculo.java
@@ -6,23 +6,26 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Veiculo {
+    // declarando variaveis
     private String cliente;
     private String telefone;
     private String placa;
     private String modelo;
     private LocalDateTime horarioDeEntrada;
 
+    // recebe os dados e define as variaveis
     public Veiculo(String cliente, String telefone , String placa, String modelo) {
         this.cliente = cliente;
         this.telefone = telefone;
         this.placa = placa;
         this.modelo = modelo;
-        this.horarioDeEntrada = LocalDateTime.now();
+        this.horarioDeEntrada = LocalDateTime.now(); // pega a data e o horario atual
     }
 
+    // transforma em uma String separada por ";"
     public String transformarEmString(){
         String retorno = this.cliente + ";" + this.telefone + ";" + this.placa + ";" + this.modelo + ";" + this.horarioDeEntrada.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        return retorno;
+        return retorno; // retorna a String
     }
 
 
